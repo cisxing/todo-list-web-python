0744dfc461d9a29304df73a30696c24c1395532d
Demo User <demouser@n228-227.mtholyoke.edu>
2016-01-04 11:21:58 -0800

    pass the mvc

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,21 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
 # Create your tests here.
-# Get into the idea of the testing loop
-# We will do some exercises
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
-        
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+# Model view controller
+# Three conceptual segments
+# Model: All the data for the application, for now strings, for later, maybe database
+# View: A bunch of HTML today
+# Controller: Logic
+# URL: universal resources locator
+# for Django: http:// protocol , other ones: https and fip
+# domain name: ourapp.io for 2016
+# what about the part /list/ABC123 -> unique indicator(id) : ABC123 and ABP: list
