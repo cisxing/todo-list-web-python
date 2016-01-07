b03faaf2ff124a0df9ceb88b69f000bab45272be
Demo User <demouser@n229-217.mtholyoke.edu>
2016-01-07 11:47:13 -0800

    add urls

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -7,12 +7,13 @@ def home_page(request):
 def new_list(request):
     new_list = List.objects.create()
     Item.objects.create(text= request.POST['item_text'],  list = new_list)
-    return redirect('/lists/the-only-list/')
+    return redirect('/lists/%d/'%(new_list.id, ))
     #we could say that
     #if request.method == 'POST':
     #return HttpResponse(request.POST[item_text])
     #but then we break other things
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list = list_)
     return render(request, 'list.html', {'items': items,
     })
