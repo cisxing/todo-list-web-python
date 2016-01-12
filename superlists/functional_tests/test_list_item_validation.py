ecdc12f470bd4674fd62eeaa961e52de138b7670
Demo User <demouser@n229-41.mtholyoke.edu>
2016-01-12 07:31:33 -0800

    added functional test for no empty validation

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -6,9 +6,12 @@ from selenium.webdriver.common.keys import Keys
 from .base import TodoFunctionalTest
 
 class ItemValidationTest(TodoFunctionalTest):
-    @skip("Haven't implemented this")
     def test_cannot_add_empty_list_item(self):
-        self.fail("finish the test")
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('')
+
+        error = self.browser.find_element_by_css_selector(".has_error")
+        self.assertEqual(error.text, "You can't have an empty list item")
         #Edith goes to the home page, and accidentally tries to
         #submit an empty list item.
         #she hits enter on the empty input box
@@ -18,7 +21,13 @@ class ItemValidationTest(TodoFunctionalTest):
 
         #she tries again with same text for the item
         #which now works
-
+        self.enter_a_new_item("Buy Milk")
+        self.check_for_row_in_list_table('1. Buy Milk')
         #perversely tries to add a second blank item
         #she receives a similar warning on the list page.
         #And she can correct it by filling some text in.
+        self.enter_a_new_item('')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        self.enter_a_new_item('Make Tea')
+        self.check_for_row_in_list_table("1. Buy Milk")
+        self.check_for_row_in_list_table("2. Make Tea")
