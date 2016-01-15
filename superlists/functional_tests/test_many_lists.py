4e9eaa60aaeb4062124b81c9f60900d628235edd
Demo User <demouser@n229-253.mtholyoke.edu>
2016-01-15 08:36:26 -0800

    add list name functional test passing

--- superlists/functional_tests/test_many_lists.py
+++ superlists/functional_tests/test_many_lists.py
@@ -2,7 +2,9 @@ from .base import TodoFunctionalTest
 
 class ManyListsTest(TodoFunctionalTest):
     def change_list_name(self, list_name):
-        pass
+        inputbox = self.browser.find_element_by_id("id_rename_list")
+        inputbox.clear()
+        inputbox.send_keys(list_name +"\n")
     def test_can_create_and_view_many_lists(self):
         #List1. Groceries
         #List2. Homework
@@ -20,13 +22,13 @@ class ManyListsTest(TodoFunctionalTest):
         self.browser.get(self.live_server_url)
         self.check_for_row_in_list_table('Groceries')
         #Edith creates a new list for her art history Homework
-        self.browser.enter_a_new_item('Read Camille')
+        self.enter_a_new_item('Read Camille')
         #Edith opens the homepage later and sees both lists
         self.browser.get(self.live_server_url)
         self.check_for_row_in_list_table('Groceries')
         self.check_for_row_in_list_table('Read Camille')
         #Edith goes to the grocery list and sees what she needs to buy
-        rows = self.browser.find_table_row('Groceries')
-        row = self.find_element_by_tag_name('a').click()
+        rows = self.find_table_row('Groceries')
+        rows.find_element_by_tag_name('a').click()
         self.check_for_row_in_list_table("Buy milk")
         self.check_for_row_in_list_table("Buy cheese")
